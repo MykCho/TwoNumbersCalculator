bf41a5880fcf9c276156b86072230b8f75b0cc2f
MykCho <137506809+MykCho@users.noreply.github.com>
2023-11-27 01:51:55 +0100

    Added preliminary fancy cleanup routine

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace TwoNumbersCalculator
+﻿using System;
+
+namespace TwoNumbersCalculator
 {
     internal class Program
     {
@@ -11,16 +13,34 @@
             Console.WriteLine("<number1> <operation> <number2>");
             Console.WriteLine("(spaces are not necessary, but preferable for the sake of beauty and Universal balance).");
             Console.WriteLine("Allowed operations: + - * / ^ !");
-            Console.WriteLine("note: factorial is a one-number operation, f.e. !12)");
+            Console.WriteLine("note: factorial is a one-number operation, f.e. 123!)");
             Console.WriteLine($"{borderLine}");
         }
 
         static void Main(string[] args)
         {
+            char[] allowedCharacters = ['+', '-', '*', '/', '^', '!', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; //not sure if needed
+            char[] allowedOperations = ['+', '-', '*', '/', '^', '!'];
             string inputString = "";
+            
+            
             ShowInitialMenu();
             inputString = Console.ReadLine();
 
-        }
+            if (inputString != "") {
+
+                for (int i = 0; i < inputString.Length; i++) //removing garbage
+                {
+                    if (!allowedCharacters.Contains(inputString[i])) {
+                        inputString = inputString.Remove(i--,1);
+                        
+                        Console.Write("\r"+inputString+" ");
+                        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(0.2));
+
+                    }
+                }
+                Console.WriteLine("\n");
+            } else { Console.WriteLine("You entered an empty string"); }
+        }   
     }
 }
